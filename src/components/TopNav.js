cceb32cb0a37f51b1b17321b680a0545bffe2406
Patrick Tobias Fischer <mail@fischerkinder.de>
2021-11-12 19:40:50 +0100

    Added some SVG Icons for the NavBar

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,6 +3,8 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import { InputGroup } from "react-bootstrap";
 
 const TopNav = () => {
 	return (
@@ -10,17 +12,34 @@ const TopNav = () => {
 			<Navbar className="expand-gl bg-light border" >
 				<Container fluid>
 					<Navbar.Brand href="#home">
-						<img src="/res/icons/logo.png"></img> {' '}
+						<img src="/res/icons/logo.png" alt="XRevent logo"></img> {' '}
 						XRevent Creator
 					</Navbar.Brand>
-					<Nav className="justify-content-center "  >
-						<Form>
-							<Form.Control className="form-control form-control-sm" style={{width: 400}}/>
-						</Form>
-					</Nav>
-					<Nav>
-						<button className="btn btn-outline-secondary btn-sm">Save</button>
-					</Nav>
+
+					<Form.Group className="justify-content-center" fluid  >
+						<InputGroup >
+							<Form.Control className="form-control form-control-sm" style={{ width: 280 }} placeholder="Enter scene Id to load ..." value="0f8fad5b-d9cb-469f-a165-70867728950e" />
+							<span class="input-group-text" >
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+									<path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
+								</svg>
+							</span>
+							<span class="input-group-text" >
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+									<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"></path>
+									<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"></path>
+								</svg>
+							</span>
+							<span class="input-group-text" >
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+									<path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+								</svg>
+							</span>
+						</InputGroup>
+					</Form.Group>
+
+					<button className="btn btn-outline-secondary btn-sm">Save</button>
+
 				</Container>
 			</Navbar>
 		</>
